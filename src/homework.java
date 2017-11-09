619abf9375df27cb6f91d9a2a4c376801c50792e
Nikita Tarakhtii <31707552+NickTar17@users.noreply.github.com>
2017-11-09 20:39:40 +0200

    First hw

---
+++ src/homework.java
@@ -0,0 +1,7 @@
+public class homework {
+     public static void main(String[] args) {
+         System.out.println("Как я провел свое лето ? \nОтлично");
+            //System.out.println("отлично");
+        }
+    }
+
